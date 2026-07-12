67bef1466393bec5480b324f131fcc195e529b9d
Saksham Shil <sakshamplzcode@gmail.com>
2026-07-12 12:48:46 +0530

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="track-cli",
-    version="0.1.0",
+    version="0.2.0",
     author="sakshamshil",
     author_email="your.email@example.com",
     description="A simple CLI tool for tracking time spent on tasks",
